e5ea06d460638925a0fd6cd9d52a0c776b9c4129
paulh16 <paulh16@gmail.com>
2014-11-06 15:15:33 -0600

    Implement is_uppercase?

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,9 +1,17 @@
 class Bob
   def initialize
-  
+
   end
 
   def hey(message)
-    'Whatever.'
+    if is_uppercase?(message)
+      'Woah, chill out!'
+    else
+      'Whatever.'
+    end
+  end
+
+  def is_uppercase?(string)
+    string == string.upcase
   end
 end
